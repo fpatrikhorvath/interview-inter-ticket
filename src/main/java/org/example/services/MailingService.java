808bff256ea572adef01e98158ed36736ad51320
fpatrikhorvath <140433011+fpatrikhorvath@users.noreply.github.com>
2025-02-04 13:33:46 +0100

    Update MailingService.java

--- src/main/java/org/example/services/MailingService.java
+++ src/main/java/org/example/services/MailingService.java
@@ -7,11 +7,11 @@ import java.util.Random;
 
 
 public class MailingService {
-    
-    private static final String PROTOCOL = "https";
+
+    private static final String PROTOCOL          = "https";
     private static final String EMAIL_STATIC_PART = "aappiumtest";
-    private static final String EMAIL_PROVIDER = "mailsac.com";
-    private static final int RANDOM_BOUND = 30000;
+    private static final String EMAIL_PROVIDER    = "mailsac.com";
+    private static final int    RANDOM_BOUND      = 30000;
 
     private String email;
 
@@ -27,30 +27,25 @@ public class MailingService {
 
     public String getValidationLink() {
         try {
-            // Send GET request to retrieve inbox contents with the email-specific path
             Response response = RestAssured.given()
-                .when()
-                .get("/inbox/" + email); // The "/inbox" path is added in the request
+                    .when()
+                    .get("/inbox/" + email);
 
-            // Extract the body of the response
             final String responseBody = response.getBody().asString();
 
-            // Check if the response contains the expected link structure
             final int startIndex = responseBody.indexOf("\"links\":[\"");
             if (startIndex == -1) {
                 System.err.println("Links not found in the response body");
                 return null;
             }
-            
+
             final int endIndex = responseBody.indexOf("\"]", startIndex);
             if (endIndex == -1) {
                 System.err.println("Invalid response format: No closing bracket found for links");
                 return null;
             }
 
-            // Extract and return the link
-            String validationLink = responseBody.substring(startIndex + 9, endIndex); // Extract link from within quotes
-            return validationLink;
+            return responseBody.substring(startIndex + 9, endIndex);
         } catch (Exception e) {
             System.err.println("Error occurred while fetching validation link: " + e.getMessage());
             return null;
